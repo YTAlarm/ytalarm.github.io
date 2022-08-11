40dd553c11a62f0d39f8e419940fc5c9f98356d3
akoskoc <akoskoc@gmail.com>
2022-08-11 12:50:37 +0200

    utils isvalidplaylist

--- src/app/services/utils.service.ts
+++ src/app/services/utils.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { VideoMetadata } from './settings.service';
 
 @Injectable({
   providedIn: 'root',
@@ -12,5 +13,42 @@ export class UtilsService {
     } catch (err) {
       return url;
     }
+  }
+
+  validPlaylist(variable: any): boolean {
+    if (!Array.isArray(variable)) {
+      return false;
+    }
+
+    if (!variable.length) {
+      return false;
+    }
+
+    for (let item of variable) {
+      if (!this.validVideoMetadata(item)) {
+        return false;
+      }
+    }
+
+    return true;
+  }
+
+  validVideoMetadata(variable: any): boolean {
+    if (!(variable instanceof Object)) {
+      return false;
+    }
+    if (typeof variable.thumbnailUrl !== 'string') {
+      return false;
+    }
+    if (typeof variable.videoId !== 'string') {
+      return false;
+    }
+    if (typeof variable.id !== 'string') {
+      return false;
+    }
+    if (typeof variable.loaded !== 'boolean') {
+      return false;
+    }
+    return true;
   }
 }
